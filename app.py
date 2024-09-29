0fa74509bd144b76e305ab34a016d7290971b964
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-09-29 18:40:15 +0100

    Calling class
    ...  with functions that will be declared in future

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import ast
 import os
 
+class PythonAnalyzer(ast.NodeVisitor):
+    def __init__(self):
+        self.imports = []
+        self.functions = []
+        self.classes = []
+        self.variables = []
+        self.function_calls = []
