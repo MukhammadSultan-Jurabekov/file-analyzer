60218556de821e54aa94d8a3ff807593d6da33c4
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-09-29 18:41:27 +0100

    Visiting calls

--- app.py
+++ app.py
@@ -25,3 +25,25 @@ class PythonAnalyzer(ast.NodeVisitor):
         for arg in node.args.args:
             self.variables.append(arg.arg)
         
+        # Analyzing function calls within functions
+        FunctionCallVisitor(self.function_calls).visit(node)
+        self.generic_visit(node)
+
+    def visit_ClassDef(self, node):
+        class_info = {"name": node.name, "methods": []}
+        self.classes.append(class_info)
+        
+        for body_item in node.body:
+            if isinstance(body_item, ast.FunctionDef):
+                class_info["methods"].append(body_item.name)
+                self.variables.extend(arg.arg for arg in body_item.args.args)
+                
+                # Analyze function calls within methods
+                FunctionCallVisitor(self.function_calls).visit(body_item)
+        self.generic_visit(node)
+
+    def visit_Assign(self, node):
+        if isinstance(node.targets[0], ast.Name):
+            self.variables.append(node.targets[0].id)
+        self.generic_visit(node)
+
